c366749368123faf23f02c6e077355b6f522c212
KaungMyat <aungakm667@gmail.com>
2025-04-15 01:37:38 +0700

    the first production ready next js template for konbaungplayingcards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,6 +23,9 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
+          <li className="tracking-[-.01em]">
+            Konbaung Desk production in ready next js template
+          </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
