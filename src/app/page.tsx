4004ebd60af223759792d89f3fedbf86107c608e
KaungMyat <aungakm667@gmail.com>
2025-04-15 02:11:11 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,19 +13,10 @@ export default function Home() {
           priority
         />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-          <li className="tracking-[-.01em]">
+          <li className="tracking-[-.01em] text-lg">
             Konbaung Desk production in ready next js template
           </li>
+          <p>By kgmyat</p>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
@@ -87,7 +78,7 @@ export default function Home() {
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://kgmyat.vercel.app"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -98,7 +89,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Go to developer website kgmyat.vercel.app →
         </a>
       </footer>
     </div>
