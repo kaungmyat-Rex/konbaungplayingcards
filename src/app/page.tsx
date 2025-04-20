d400d661c810e9a7317c5b89368cfa9912d15156
KaungMyat <aungakm667@gmail.com>
2025-04-21 03:26:41 +0700

    fix margin and padding in contact

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { FaLocationDot } from "react-icons/fa6";
 import { FaSquareInstagram } from "react-icons/fa6";
 import { SiGmail } from "react-icons/si";
 import { CgProfile } from "react-icons/cg";
+import Link from "next/link";
 export default function Home() {
   return (
     <main className="h-screen snap-y snap-mandatory overflow-y-scroll scroll-smooth">
@@ -23,49 +24,56 @@ export default function Home() {
             height={1200}
             className="w-full h-full object-cover"
           />
-          <h4 className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] text-white font-Montserrat font-semibold text-4xl md:text-5xl">
+          <h4 className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] text-white font-Montserrat font-semibold text-4xl md:text-5xl whitespace-nowrap">
             Contact Me
           </h4>
         </div>
-        <div className="flex justify-center items-center -mt-10 md:mt-10 md:px-30 gap-x-10 md:gap-x-20">
-          <div className="flex flex-col justify-center items-center gap-y-10 md:flex-row md:gap-x-28">
+        <div className="flex justify-center items-start md:items-center -mt-10 md:mt-10 px-5 md:px-30 gap-x-10 md:gap-x-20">
+          <div className="flex flex-col justify-center items-center md:gap-y-10 gap-y-[60px] md:flex-row md:gap-x-28">
             <div className="flex flex-col justify-center items-center gap-y-1 ">
-              <CgProfile size={40} color="white" />
-              <p className="font-SourceSerif text-white font-semibold text-lg">
+              <CgProfile color="white" className="text-xl" />
+              <p className="font-SourceSerif text-white font-semibold text-base md:text-lg">
                 CREATOR
               </p>
-              <p className="text-white font-Nunito font-normal text-base whitespace-nowrap">
+              <p className="text-white font-Nunito font-normal text-sm whitespace-nowrap">
                 Han Win Htet
               </p>
             </div>
             <div className="flex flex-col justify-center items-center gap-y-1">
-              <FaLocationDot size={40} color="white" />
-              <p className="font-SourceSerif text-white font-semibold text-lg">
+              <FaLocationDot color="white" className="text-xl" />
+              <p className="font-SourceSerif text-white font-semibold text-base md:text-lg">
                 ADDRESS
               </p>
-              <p className="text-white font-Nunito font-normal text-base whitespace-nowrap">
+              <p className="text-white font-Nunito font-normal text-sm whitespace-nowrap">
                 Bangkok / Thailand
               </p>
             </div>
           </div>
           <div className="flex flex-col justify-center items-center gap-y-10 md:flex-row md:gap-x-28">
             <div className="flex flex-col justify-center items-center gap-y-1">
-              <SiGmail size={40} color="white" />
-              <p className="font-SourceSerif text-white font-semibold text-lg">
+              <SiGmail color="white" className="text-xl" />
+              <p className="font-SourceSerif text-white font-semibold text-base md:text-lg">
                 EMAIL
               </p>
-              <p className="text-white font-Nunito font-normal text-base">
+              <Link
+                href="mailto:thisishanwinhtet@gmail.com"
+                className="text-white font-Nunito font-normal text-sm wrap-break-word w-[68%] md:w-[100%]"
+              >
                 thisishanwinhtet@gmail.com
-              </p>
+              </Link>
             </div>
             <div className="flex flex-col justify-center items-center gap-y-1">
-              <FaSquareInstagram size={40} color="white" />
-              <p className="font-SourceSerif text-white font-semibold text-lg">
+              <FaSquareInstagram color="white" className="text-xl" />
+              <p className="font-SourceSerif text-white font-semibold text-base md:text-lg">
                 INSTAGRAM
               </p>
-              <p className="text-white font-Nunito font-normal text-base">
+              <Link
+                href="https://www.instagram.com/hansolo_2023/"
+                target="_blank"
+                className="text-white font-Nunito font-normal text-sm"
+              >
                 hansolo_2023
-              </p>
+              </Link>
             </div>
           </div>
         </div>
